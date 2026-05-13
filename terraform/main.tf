5858d8f1f0ed073d5043e428d61c793f5b34424d
Susmitha-18 <susmithasivakumar1832006@gmail.com>
2026-05-13 16:00:33 +0530

    Added Jenkins pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "jenkins_server" {
+ami           = "ami-0f5ee92e2d63afc18"
+instance_type = "t3.micro"
+key_name = "Susmipass"
+tags = {
+Name = "Jenkins-Server"
+}
+}
+
+resource "aws_instance" "dev_server" {
+ami           = "ami-0f5ee92e2d63afc18"
+instance_type = "t3.micro"
+key_name = "Susmipass"
+
+tags = {
+Name = "Dev-Server"
+}
+}
+
+resource "aws_instance" "prod_server" {
+ami           = "ami-0f5ee92e2d63afc18"
+instance_type = "t3.micro"
+key_name = "Susmipass"
+tags = {
+Name = "Prod-Server"
+}
+}
